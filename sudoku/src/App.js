900871e5515bd3b90498ae63fd9ecfefc2ea30f2
bfp4 <66973969+bfp4@users.noreply.github.com>
2021-12-31 01:20:41 -0500

    nice board set-up

--- sudoku/src/App.js
+++ sudoku/src/App.js
@@ -2,13 +2,13 @@ import React from "react"
 
 import { BoardProvider } from "./Context/BoardContext";
 import Board from "./components/Board/Board"
+import SolveButton from "./components/SolveButton/SolveButton";
 
 function App() {
   return (
     <BoardProvider>
-      <div>
         <Board />
-      </div>
+        <SolveButton />
     </BoardProvider>
   );
 }
---
+++ sudoku/src/components/SolveButton/SolveButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SolveButton() {
+    return (
+        <button>
+            Solve
+        </button>
+    )
+}
